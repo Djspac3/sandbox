b0a3662a8be39ded7c089c88198c6fe77150b23d
dj friedman <buildingwithdj+git@gmail.com>
2025-11-27 15:51:52 -0500

    setup cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-				additionalPrerenderRoutes: ['/404'],
-				previewMiddlewareEnabled: true,
-				previewMiddlewareFallback: '/404',
-			},
-		}),
-	],
+  plugins: [
+    preact({
+      prerender: {
+        enabled: true,
+        renderTarget: "#app",
+        additionalPrerenderRoutes: ["/404"],
+        previewMiddlewareEnabled: true,
+        previewMiddlewareFallback: "/404",
+      },
+    }),
+    cloudflare(),
+  ],
 });
